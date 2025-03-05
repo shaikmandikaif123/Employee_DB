2e74a857a2703234245a0a453b41e6b2320479c0
shaikmandikaif123 <140751239+shaikmandikaif123@users.noreply.github.com>
2025-03-05 23:11:00 +0530

    Delete New folder directory

--- New folder/CreditCardApplication/src/main/java/org/example/Abstraction/ApplicationStatusManager.java
+++
@@ -1,8 +0,0 @@
-package org.example.Abstraction;
-
-// Abstract class for application status management (Abstraction)
-public abstract class ApplicationStatusManager {
-    public abstract void approveApplication();
-    public abstract void rejectApplication();
-}
-
